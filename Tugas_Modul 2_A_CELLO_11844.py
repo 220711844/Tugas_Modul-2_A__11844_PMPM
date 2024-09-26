482d03c815d2fe106934a560aeaf52f90158092e
Cello <220711844@students.uajy.ac.id>
2024-09-26 15:49:50 +0700

    Update Tugas_Modul 2_A_CELLO_11844.py

--- Tugas_Modul 2_A_CELLO_11844.py
+++ Tugas_Modul 2_A_CELLO_11844.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pickle
 import os
 
-model_path = 'D:\semester 5\Pembelajaran Mesin dan Mendalam\modul 2\Tugas_A_11844'
-model=os.path.join(model_path,'GBT_heartDisease_model.pkl')
+model='GBT_heartDisease_model.pkl')
 
 with open(model,'rb') as f:
     loaded_model = pickle.load(f)
